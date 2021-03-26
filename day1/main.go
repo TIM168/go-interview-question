39f026312dbbc44b4ecf3fd6f7f33e1f7ca5eccc
Tim <oncwnuHbZSecoWojqpjf_P4InxD8@git.weixin.qq.com>
2021-03-26 17:15:43 +0800

    day1

--- day1/main.go
+++ day1/main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("first")
+	defer_call()
+}
+
+func defer_call() {
+	defer func() {
+		fmt.Println("打印前")
+	}()
+
+	defer func() {
+		fmt.Println("打印中")
+	}()
+
+	defer func() {
+		fmt.Println("打印后")
+	}()
+
+	panic("触发异常")
 }
