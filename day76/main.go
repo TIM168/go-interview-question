8f61b93b3114ea3176e9444c8579f5e28b196bfe
Tim <oncwnuHbZSecoWojqpjf_P4InxD8@git.weixin.qq.com>
2021-04-09 18:09:34 +0800

    unit day77

--- day76/main.go
+++ day76/main.go
@@ -1,7 +1,69 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
 
+type S1 struct {
+}
+
+func (s1 S1) f() {
+	fmt.Println("S1.f()")
+}
+
+func (s1 S1) g() {
+	fmt.Println("S1.g()")
+}
+
+type S2 struct {
+	S1
+}
+
+func (s2 S2) f() {
+	fmt.Println("S2.f()")
+}
+
+type I interface {
+	f()
+}
+
+/**
+类型断言，结构体嵌套。
+结构体 S2 嵌套了结构体 S1，S2 自己没有实现 g() ，调用的是 S1 的 g()
+ */
+func printType(i I) {
+	fmt.Printf("%T\n", i)
+	if s1, ok := i.(S1); ok {
+		s1.f()
+		s1.g()
+	}
+	if s2, ok := i.(S2); ok {
+		s2.f()
+		s2.g()
+	}
+}
+
+func test1() {
+	printType(S1{})
+	printType(S2{})
+}
+
+/**
+协程里面，使用 wg.Add(1) 但是没有 wg.Done()，导致 panic()
+ */
+func test2() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		fmt.Println("1")
+		wg.Done()
+		wg.Add(1)  // 1
+	}()
+	wg.Wait()
+}
 
 func main() {
-	
+	test1()
+	test2()
 }
---
+++ day77/main.go
@@ -0,0 +1,16 @@
+package main
+
+/**
+1.关于 cap 函数适用下面哪些类型？
+
+A. 数组；
+B. channel;
+C. map；
+D. slice；
+
+
+ */
+
+func main() {
+	
+}
