3e9b9b9cecc234b0b06b021885139b54dfa1743d
Tim <oncwnuHbZSecoWojqpjf_P4InxD8@git.weixin.qq.com>
2021-04-13 16:13:05 +0800

    unit day100

--- day100/main.go
+++ day100/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
+/**
+参考答案即解析：按字母无序输出。知识点：遍历 map 是无序的
+ */
 func test1() {
 	m := map[string]int{
 		"G": 7, "A": 1,
@@ -17,8 +23,52 @@ func test1() {
 	fmt.Println(order)
 }
 
+type UserAges struct {
+	ages map[string]int
+	sync.Mutex
+}
 
+func (ua *UserAges) Add(name string, age int) {
+	ua.Lock()
+	defer ua.Unlock()
+	ua.ages[name] = age
+}
+
+func (ua *UserAges) Get(name string) int {
+	if age, ok := ua.ages[name]; ok {
+		return age
+	}
+	return -1
+}
+
+/**
+参考答案即解析：在执行 Get() 方法时可能报错。知识点：读写锁。虽然可以使用 sync.Mutex 做写锁，
+但是 map 是并发读写不安全的。map 属于引用类型，并发读写时多个协程是通过指针访问同一个地址，即访问共享变量，此时同时读写资源存在竞争关系，会报错 “fatal error: concurrent map read and map write”
+ */
+func test2() {
+	count := 1000
+	gw := sync.WaitGroup{}
+	gw.Add(count * 3)
+	u := UserAges{ages: map[string]int{}}
+	add := func(i int) {
+		u.Add(fmt.Sprintf("user_%d", i), i)
+		gw.Done()
+	}
+	for i := 0; i < count; i++ {
+		go add(i)
+		go add(i)
+	}
+	for i := 0; i < count; i++ {
+		go func(i int) {
+			defer gw.Done()
+			u.Get(fmt.Sprintf("user_%d", i))
+		}(i)
+	}
+	gw.Wait()
+	fmt.Println("Done")
+}
 
 func main() {
 	test1()
+	test2()
 }
