792c0efb424f47909a930ad9a6d94e8716faebf9
Tim <oncwnuHbZSecoWojqpjf_P4InxD8@git.weixin.qq.com>
2021-04-14 18:07:21 +0800

    unit day109

--- day108/main.go
+++ day108/main.go
@@ -8,8 +8,15 @@ import (
 
 type People struct {
 	name string `json:"name"`
+	//Name string `json:"name"`
 }
 
+/**
+参考答案及解析：people:{}。
+按照 go 的语法，小写开头的方法、属性或 struct 是私有的，
+同样，在 json 解码或转码的时候也无法实现私有属性的转换
+这段代码是无法正常得到 People 的 name 值的。而且，私有属性 name 也不应该加 json 的标签
+ */
 func test1() {
 	js := `{
 		"name":"11"
@@ -26,9 +33,12 @@ func test1() {
 var ip string
 var port int
 
+/**
+补充 A、B 两处代码，实现程序能解析 ip 和 prot 参数，默认值是  0.0.0.0 和 8000
+ */
 func init() {
-	flag.StringVar(&ip,"ip","0.0.0.0","ip address")
-	flag.IntVar(&port,"port",8000,"port number")
+	flag.StringVar(&ip,"ip","0.0.0.0","ip address") //A
+	flag.IntVar(&port,"port",8000,"port number") //B
 }
 
 func test2() {
--- day109/main.go
+++ day109/main.go
@@ -1,5 +1,47 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+/**
+参考答案及解析：panic。
+协程开启还未来得及执行，chan 就已经 close() ，往已经关闭的 chan 写数据会 panic
+ */
+func test1() {
+	ch := make(chan int, 1000)
+	go func() {
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
+	}()
+	go func() {
+		for {
+			a, ok := <-ch
+			if !ok {
+				fmt.Println("close")
+				return
+			}
+			fmt.Println("a: ", a)
+		}
+	}()
+	close(ch)
+	fmt.Println("ok")
+	time.Sleep(time.Second * 1)
+}
+
+type S struct {
+	v int
+}
+
+func test2() {
+	s := []S{{1}, {3}, {5}, {2}}
+	//A
+	fmt.Printf("%#v", s)
+
+}
+
 func main() {
-	
+	test1()
 }
