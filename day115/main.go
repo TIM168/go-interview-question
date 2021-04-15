bb15013f2e04ad2750b84887f349e03b8b08462b
Tim <oncwnuHbZSecoWojqpjf_P4InxD8@git.weixin.qq.com>
2021-04-15 17:31:11 +0800

    unit day116

--- day115/main.go
+++ day115/main.go
@@ -1,7 +1,56 @@
 package main
 
+import "fmt"
+
+var y int
+
+func f(x int) int {
+	return 7
+}
+
+/**
+
+1.下面正确的是？
+
+A.
+switch y = f(2) {
+case y == 7:
+  return
+}
+
+B.
+switch y = f(2); {
+case y == 7:
+  return
+}
+
+C.
+switch y = f(2) {
+case 7:
+  return
+}
+
+D.
+switch y = f(2); {
+case 7:
+  return
+}
+
+
+ */
+
+func test1() {
+	a := []int{1,2,3,4}
+	b := variadic(a...)
+	b[0],b[1] = b[1],b[0]
+	fmt.Println(a)
+}
+
+func variadic(ints ...int) []int {
+	return ints
+}
 
 
 func main() {
-	
+	test1()
 }
